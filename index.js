e569ce1ae7e7d798bfa19cdb951a47d47ad256f2
codewithjulie <codewithjulie@gmail.com>
2020-09-26 18:11:18 -0700

    update for heroku

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ io.on('connection', socket => {
   })
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
